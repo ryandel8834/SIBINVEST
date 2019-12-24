fbfc5d758ff63f0809b44f26252220b97efd512f
ryandel8834 <ryandel8834@gmail.com>
2019-12-24 21:52:44 +0100

    toggle Navbar depending on url in progress

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,8 +1,5 @@
 <template>
  <b-container class="p-0">
-  <div class="ribbon d-flex align-items-end">
-    <img src="../../assets/logo.png" alt="">
-  </div>
   <b-navbar class="navbar p-0" toggleable="lg" type="dark" fixed="top" @scroll="handleScroll">
     <b-container class="px-0">
       <b-navbar-brand
@@ -22,9 +19,9 @@
       <b-navbar-nav class="ml-auto" hidden-xs>
         <b-nav-item text="Lang" right> <router-link to="/"> pocetna</router-link> </b-nav-item>
         <b-nav-item text="Lang" right href="#about-us">o nama</b-nav-item>
-        <b-nav-item text="Lang" right> <router-link to="/reference"> reference </router-link> </b-nav-item>
+        <b-nav-item text="Lang" right href="#references" v-on:click="toggleNavbar"> reference </b-nav-item>
         <b-nav-item text="Lang" right href="#projects-in-progress">projekti u toku</b-nav-item>
-        <b-nav-item text="Lang" right> <router-link to="/#contact-us" href="#contact-us"> kontakt </router-link> </b-nav-item>
+        <b-nav-item text="Lang" right href="#contact-us"> konkakt </b-nav-item>
         <b-nav-item text="Lang" right>search</b-nav-item>
       </b-navbar-nav>
     </b-container>
@@ -50,11 +47,23 @@ export default {
   },
   methods: {
     handleScroll(event) {
-      let header = document.querySelector(".navbar");
-      if(window.scrollY > 480) {
+      if (this.$route.path !== "/reference") {
+        let header = document.querySelector(".navbar");
+        if(window.scrollY > 480) {
+          header.classList.add("sticky-header");
+          this.logoName = "logo1.jpg";
+        } else if (window.scrollY < 480) {
+          header.classList.remove("sticky-header");
+          this.logoName = "";
+        }
+      }
+    },
+    toggleNavbar() {
+      if (this.$route.path === "/reference") {
+        let header = document.querySelector(".navbar");
         header.classList.add("sticky-header");
         this.logoName = "logo1.jpg";
-      } else if (window.scrollY < 480) {
+      } else {
         header.classList.remove("sticky-header");
         this.logoName = "";
       }
@@ -64,8 +73,19 @@ export default {
   beforeMount() {
     window.addEventListener("scroll", this.handleScroll);
   },
+  mounted() {
+    if (this.$route.path === "/reference") {
+      let header = document.querySelector(".navbar");
+      header.classList.add("sticky-header");
+      this.logoName = "logo1.jpg";
+    } else {
+      header.classList.remove("sticky-header");
+      this.logoName = "";
+    }
+  },
   beforeDestroy() {
     window.removeEventListener("scroll", this.handleScroll);
+    toggleNavbar();
   }
 };
 </script>
@@ -87,20 +107,6 @@ export default {
 .navbar-dark .navbar-nav .nav-link:hover,
 .navbar-dark .navbar-nav .nav-link a:hover {
   text-decoration: none;
-}
-.sticky-top {
-  top: 20px;
-}
-.ribbon {
-  position: absolute;
-  top: 0;
-  height: 60vh;
-  width: 300px;
-  background: rgb(87, 88, 90);
-}
-.ribbon img {
-  width: 100%;
-  padding: 25px 40px 20px 40px;
 }
 .navbar.sticky-header {
   height: 70px;
